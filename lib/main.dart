815ac35ff64621934f6d948b3595f0b6797e4edc
paulotbag2@gmail.com <paulotbag2@gmail.com>
2024-06-12 16:12:24 -0400

    Lab of week 4 - login page from lab 2 with EncriptedSharedPreferences to save previous user login data

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:encrypted_shared_preferences/encrypted_shared_preferences.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,17 +6,14 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -24,9 +22,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -35,141 +31,141 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late TextEditingController _controller;
+  late TextEditingController _controller1;
+  String imageSource = "images/question-mark.png"; // Default image source
+  late EncryptedSharedPreferences storedData;
 
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+    _controller1 = TextEditingController();
+    storedData = EncryptedSharedPreferences();
 
+    _loadSavedCredentials();
+  }
+
+  Future<void> _loadSavedCredentials() async {
+    final savedUsername = await storedData.getString("username");
+    final savedPassword = await storedData.getString("password");
+
+    if (savedUsername != null && savedPassword != null) {
+      _controller.text = savedUsername;
+      _controller1.text = savedPassword;
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Previous login name and password loaded'),
+          action: SnackBarAction(
+            label: 'Clear Saved Data',
+            onPressed: (){
+              storedData.remove("username");
+              storedData.remove("password");
+              _controller.text = "";
+              _controller1.text = "";
+
+            },
+          ),
+        ),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _controller1.dispose();
+    super.dispose();
+  }
+
+  void buttonClicked() {
+    String password = _controller1.text.trim();
+    if (password == "QWERTY123") {
+      setState(() {
+        imageSource = "images/idea.png"; // Change image to light bulb
+      });
+    } else {
+      setState(() {
+        imageSource = "images/stop.png"; // Change image to stop sign
+      });
+    }
+    _showSaveDialog();
+  }
+
+  void _showSaveDialog() {
+    showDialog<String>(
+      context: context,
+      builder: (BuildContext context) =>
+          AlertDialog(
+            title: const Text('Save Credentials'),
+            content: const Text(
+                'Would you like to save your username and password for the next time?'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  _saveCredentials();
+                  Navigator.pop(context);
+                },
+                child: const Text('Yes'),
+              ),
+              TextButton(
+                onPressed: () {
+                  storedData.remove("username");
+                  storedData.remove("password");
+                  _controller.text = "";
+                  _controller1.text = "";
+                  Navigator.pop(context);
+                },
+                child: const Text('No'),
+              ),
+            ],
+          ),
+    );
+  }
+
+  Future<void> _saveCredentials() async {
+    await storedData.setString("username", _controller.text);
+    await storedData.setString("password", _controller1.text);
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        title: Text(widget.title),
+      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Padding(padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 15.0) ,// Space between elements
-              child: Text(
-                "BROWSE CATEGORIES",
-                style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                hintText: "Type here",
+                border: OutlineInputBorder(),
+                labelText: "Login",
               ),
+              obscureText: true,
             ),
-            Padding(padding: EdgeInsets.only(left: 15.0) ,// Alligned to the left side
-              child: Text(
-                "Not sure about exactly which recipe you're looking for? Do a search, or dive into our most popular categories.",
-                style: TextStyle(fontSize: 11.0),
+            TextField(
+              controller: _controller1,
+              decoration: InputDecoration(
+                hintText: "Type here",
+                border: OutlineInputBorder(),
+                labelText: "Password",
               ),
+              obscureText: true,
             ),
-            Padding(padding: EdgeInsets.symmetric(horizontal: 15.0) ,// Space between elements
-              child: Text(
-                "BY MEAT",
-                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-              ),
-            ), //
-            Row( mainAxisAlignment: MainAxisAlignment.spaceAround, //FIRST ROW OF IMAGES
-                children: [
-                  Stack( alignment:  AlignmentDirectional.center, children: <Widget>[
-                    ClipRRect( borderRadius: BorderRadius.circular(80),
-                      child:
-                      Image.asset("images/pexels-catscoming-2942319.jpg", width: 80, height: 80, fit: BoxFit.cover,),
-                    ),
-                    Text("BEEF", style: TextStyle(fontSize: 15.0, backgroundColor:Colors.transparent, color: Colors.white, fontWeight: FontWeight.bold),)
-                  ]),
-                  Stack(alignment:  AlignmentDirectional.center, children: <Widget>[
-                    ClipRRect( borderRadius: BorderRadius.circular(80),
-                      child:
-                      Image.asset("images/pexels-dana-tentis-118658-725992.jpg", width: 80, height: 80, fit: BoxFit.cover,),
-                    ),
-                    Text("CHICKEN", style: TextStyle(fontSize: 15.0, backgroundColor:Colors.transparent, color: Colors.white, fontWeight: FontWeight.bold),)
-                  ]),
-                  Stack(alignment:  AlignmentDirectional.center, children: <Widget>[
-                    ClipRRect( borderRadius: BorderRadius.circular(80),
-                      child:
-                      Image.asset("images/pexels-jeshoots-3688.jpg", width: 80, height: 80, fit: BoxFit.cover),
-                    ),
-                    Text("PORK", style: TextStyle(fontSize: 15.0, backgroundColor:Colors.transparent, color: Colors.white, fontWeight: FontWeight.bold),)
-                  ]),
-                  Stack(alignment:  AlignmentDirectional.center, children: <Widget>[
-                    ClipRRect( borderRadius: BorderRadius.circular(80),
-                      child:
-                      Image.asset("images/pexels-jill-wellington-1638660-257816.jpg", width: 80, height: 80,fit: BoxFit.cover),
-                    ),
-                    Text("PORK", style: TextStyle(fontSize: 15.0, backgroundColor:Colors.transparent, color: Colors.white, fontWeight: FontWeight.bold),)
-                  ]),
-                ]),
-            Padding(padding: EdgeInsets.symmetric(horizontal: 15.0) ,// Space between elements
-              child: Text(
-                "BY COURSE",
-                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-              ),
+            ElevatedButton(
+              onPressed: buttonClicked,
+              child: Text("Login"),
             ),
-            Row( mainAxisAlignment: MainAxisAlignment.spaceAround, //SECOND ROW OF IMAGES
-                children: [
-                  Stack(alignment:  AlignmentDirectional.bottomCenter, children: <Widget>[
-                    ClipRRect( borderRadius: BorderRadius.circular(80),
-                      child:
-                      Image.asset("images/pexels-markusspiske-112781.jpg", width: 80, height: 80, fit: BoxFit.cover),
-                    ),
-                    Text("Main Dishes", style: TextStyle(fontSize: 15.0, backgroundColor:Colors.transparent,),),
-                  ]),
-                  Stack(alignment:  AlignmentDirectional.bottomCenter, children: <Widget>[
-                    ClipRRect( borderRadius: BorderRadius.circular(80),
-                      child:
-                      Image.asset("images/pexels-pixabay-39521.jpg", width: 80, height: 80, fit: BoxFit.cover),
-                    ),
-                    Text("Salad Recipes", style: TextStyle(fontSize: 15.0, backgroundColor:Colors.transparent,),)
-                  ]),
-                  Stack(alignment:  AlignmentDirectional.bottomCenter, children: <Widget>[
-                    ClipRRect( borderRadius: BorderRadius.circular(80),
-                      child:
-                      Image.asset("images/pexels-sebastian-coman-photography-1598188-3791089.jpg", width: 80, height: 80, fit: BoxFit.cover),
-                    ),
-                    Text("Side Dishes", style: TextStyle(fontSize: 15.0, backgroundColor:Colors.transparent,),)
-                  ]),
-                  Stack(alignment:  AlignmentDirectional.bottomCenter, children: <Widget>[
-                    ClipRRect( borderRadius: BorderRadius.circular(80),
-                      child:
-                      Image.asset("images/pexels-valeriya-1251208.jpg", width: 80, height: 80, fit: BoxFit.cover),
-                    ),
-                    Text("Crokpot", style: TextStyle(fontSize: 15.0, backgroundColor:Colors.transparent,),)
-                  ]),
-                ]),
-            Padding(padding: EdgeInsets.symmetric(horizontal: 15.0) ,// Space between elements
-              child: Text(
-                "BY DESSERT",
-                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-              ),
-            ),
-            Row( mainAxisAlignment: MainAxisAlignment.spaceAround, //THIRD ROW OF IMAGES
-                children: [
-                  Stack( alignment:  AlignmentDirectional.bottomCenter, children: <Widget>[
-                    ClipRRect( borderRadius: BorderRadius.circular(80),
-                      child:
-                      Image.asset("images/pexels-pixabay-ice-cream-1.jpg", width: 80, height: 80, fit: BoxFit.cover),
-                    ),
-                    Text("Ice Cream", style: TextStyle(fontSize: 15.0, backgroundColor:Colors.transparent,),)
-                  ]),
-                  Stack(alignment:  AlignmentDirectional.bottomCenter, children: <Widget>[
-                    ClipRRect( borderRadius: BorderRadius.circular(80),
-                      child:
-                      Image.asset("images/pexels-pixabay-brownie-1.jpg", width: 80, height: 80,fit: BoxFit.cover),
-                    ),
-                    Text("Brownies", style: TextStyle(fontSize: 15.0, backgroundColor:Colors.transparent,),)
-                  ]),
-                  Stack(alignment:  AlignmentDirectional.bottomCenter, children: <Widget>[
-                    ClipRRect( borderRadius: BorderRadius.circular(80),
-                      child:
-                      Image.asset("images/pexels-pixabay-pies-1.jpg", width: 80, height: 80,fit: BoxFit.cover),
-                    ),
-                    Text("Pies", style: TextStyle(fontSize: 15.0, backgroundColor:Colors.transparent,),)
-                  ]),
-                  Stack(alignment:  AlignmentDirectional.bottomCenter, children: <Widget>[
-                    ClipRRect( borderRadius: BorderRadius.circular(80),
-                      child:
-                      Image.asset("images/pexels-pixabay-cookies-1.jpg", width: 80, height: 80,fit: BoxFit.cover),
-                    ),
-                    Text("Cookies", style: TextStyle(fontSize: 15.0, backgroundColor:Colors.transparent,),)
-                  ]),
-                ]),
+            Image.asset(
+              imageSource,
+              width: 150,
+              height: 150,
+            )
           ],
         ),
       ),
